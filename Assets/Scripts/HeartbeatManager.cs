d6ec0867a1a51bf42b0e68bd1ddaa31087772645
Abigail <asyelsub@gmail.com>
2020-06-26 21:26:48 -0400

    got heartbeatManager/bodyMonitor working, i can change the frequency and amplitude of it as a line renderer pretty easily now

---
+++ Assets/Scripts/HeartbeatManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartbeatManager : MonoBehaviour
+{
+    public GameObject heartBeat;
+    private LineRenderer lineRenderer;
+    private int lengthOfLineRenderer;
+    
+    public float amp;
+
+    public float freq;
+    //good freqs are:
+    //2.59, 3.28, 0.77, -2.16, -1.8, -1.56, -0.24, -0.08
+    //norm at 0.55
+    void Start()
+    {
+        lineRenderer = heartBeat.GetComponent<LineRenderer>();
+        lengthOfLineRenderer = lineRenderer.positionCount;
+        print(lengthOfLineRenderer);
+        
+        
+    }
+
+    private void Update()
+    {
+        
+        var t = Time.time;
+        for (int i = 0; i < lengthOfLineRenderer; i++)
+        {
+            Vector3 k = lineRenderer.GetPosition(i);
+            lineRenderer.SetPosition(i, new Vector3( k.x, Mathf.Sin((i + t) * freq)* amp + 3, 0.0f));
+        }
+    }
+}
