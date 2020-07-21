4eb48fb05574939ac38862681a11fced92a6210b
Abigail <asyelsub@gmail.com>
2020-07-20 20:43:37 -0400

    Symbols appear if the evidence is gotten? I think that's what it is. Now I need to figure out how to like connect the mouse to working prefab by prefab.

--- Assets/Scripts/CourtRecordManager.cs
+++ Assets/Scripts/CourtRecordManager.cs
@@ -9,18 +9,31 @@ public class CourtRecordManager : MonoBehaviour
 {
     [SerializeField]
     private BaseCaseLogic activeCaseLogic;
-    [HideInInspector]
-    public List<LocationBase> locationsBeingDisplayed = new List<LocationBase>();
     
     [SerializeField]
     private GameObject headerPrefab;
 
     [SerializeField] 
     private GameObject evidenceIconPrefab;
+
+    [SerializeField] 
+    private GameObject evidenceBackgroundPrefab;
+
+    [SerializeField] 
+    private Evidence inactiveEvidence;
     
+    [HideInInspector]
+    public List<LocationBase> locationsBeingDisplayed = new List<LocationBase>();
+
     [HideInInspector]
     public List<GameObject> headers = new List<GameObject>();
 
+    [HideInInspector]
+    public List<GameObject> bigListOfEvidenceIcons = new List<GameObject>();
+    
+    [HideInInspector]
+    public List<GameObject> bigListOfEvidenceBackgrounds = new List<GameObject>();
+    
     [HideInInspector]
     public bool courtRecordActive = false;
     
@@ -61,7 +74,7 @@ public class CourtRecordManager : MonoBehaviour
                 k = 0;
                 l = i;
             }
-            Vector2 headerPos = new Vector2(-220 + k, 115 - l*70);
+            Vector2 headerPos = new Vector2(-220 + k, 115 - l*80);
             GameObject activeHeaderPrefab = Instantiate(headerPrefab,gameObject.transform,false);
             activeHeaderPrefab.GetComponent<RectTransform>().anchoredPosition = headerPos;
             activeHeaderPrefab.GetComponentInChildren<Text>().text = locationsBeingDisplayed[i].locationName;
@@ -69,32 +82,43 @@ public class CourtRecordManager : MonoBehaviour
             DisplayEvidenceUnderHeader(i);
         }
         print("headers:" + headers[0].GetComponentInChildren<Text>().text + headers[1].GetComponentInChildren<Text>().text + headers[2].GetComponentInChildren<Text>().text);
+        //print("bigListOfEvidenceIcons has " + bigListOfEvidenceIcons.Count + " amount of shit in it.");
     }
 
     void DisplayEvidenceUnderHeader(int headerNumber)
     {
         for (int i = 0; i < locationsBeingDisplayed[headerNumber].evidenceAtLocation.Length; i++)
         {
+            int h = 0;
             int k = 0;
-            int l = 0;
             if (i >= 2 && locationsBeingDisplayed[headerNumber].evidenceAtLocation.Length > 2)
             {
-                k = 30;
-                l = i - 2;
+                h = 25;
+                k = i - 2;
             }
             else
             {
-                k = 0;
-                l = i;
+                h = 0;
+                k = i;
             }
-            Vector2 evidencePos = new Vector2(-30 + k,-20 - l * 30);
-            GameObject activeEvidenceIconPrefab =
-                Instantiate(evidenceIconPrefab, headers[headerNumber].transform, false);
+            Vector2 evidencePos = new Vector2(-25 + h,-25 - k * 25);
+            GameObject activeEvidenceBackground = Instantiate(evidenceBackgroundPrefab, headers[headerNumber].transform, false);
+            //bigListOfEvidenceBackgrounds.Add(activeEvidenceBackground);
+            GameObject activeEvidenceIconPrefab = Instantiate(evidenceIconPrefab, headers[headerNumber].transform, false);
+            //bigListOfEvidenceIcons.Add(activeEvidenceIconPrefab);
+            activeEvidenceBackground.GetComponent<RectTransform>().anchoredPosition = evidencePos;
             activeEvidenceIconPrefab.GetComponent<RectTransform>().anchoredPosition = evidencePos;
-            activeEvidenceIconPrefab.GetComponent<Image>().sprite =
-                locationsBeingDisplayed[headerNumber].evidenceAtLocation[i].imageInCourtRecord;
+            
+            //* This is currently set in the scriptableObjcets of evidences, *//
+            //* ideally it would be on the BaseCaseLogic. *//
+            if (locationsBeingDisplayed[headerNumber].evidenceAtLocation[i].evidenceCollected)
+            { 
+                activeEvidenceIconPrefab.GetComponent<Image>().sprite = locationsBeingDisplayed[headerNumber].evidenceAtLocation[i].imageInCourtRecord;   
+            }
+            else
+            {
+                activeEvidenceIconPrefab.GetComponent<Image>().sprite = inactiveEvidence.imageInCourtRecord;
+            }
         }
     }
-    
-    
 }
